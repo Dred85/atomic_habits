f577c1eeafcae215f8cba468ab91761a743b23ef
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-26 18:28:01 +0300

    add all

---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='csu@mail.ru',
+            first_name='dred',
+            last_name='Home',
+            is_staff=True,
+            is_active=True,
+            is_superuser=True
+        )
+
+        user.set_password('12qwe34')
+        user.save()
--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 NULLABLE = {"blank": True, "null": True}
 
 
@@ -27,11 +26,16 @@ class User(AbstractUser):
         **NULLABLE,
     )
 
+    tg_chat_id = models.CharField(max_length=50,
+                                  verbose_name='телеграм chat-id',
+                                  **NULLABLE)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
-
-
