04cc1a9210a43bf81b9310d4cba5da970ea6e73c
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-26 20:12:25 +0300

    Теперь можно зарегаться, любому пользователю

--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
 from rest_framework import generics
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers import UserSerializer
 
 
-class UserCreateAPIView(generics.CreateAPIView):
+class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (
+        AllowAny,
+    )  # Даем доступ для всех не авторизованных пользователей
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
+
