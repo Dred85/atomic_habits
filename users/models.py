3194b3405d81c77eb611d5e128cc2eade8011c1b
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-26 18:04:45 +0300

    add app habits

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="почта", help_text="Укажите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35, verbose_name="телефон", help_text="Укажите телефон", **NULLABLE
+    )
+
+    city = models.CharField(
+        max_length=50, verbose_name="город", help_text="Укажите город", **NULLABLE
+    )
+
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="аватар",
+        help_text="Загрузите аватар",
+        **NULLABLE,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
